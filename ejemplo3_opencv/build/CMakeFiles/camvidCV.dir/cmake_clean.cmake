83b93b4912b2fb413d4f5efbc9135e4da3eec4dc
JuanjoEraz0 <juanerazo.fi@gmail.com>
2024-10-06 22:01:23 -0600

    se agrega ejemplo de apertura de camara

---
+++ ejemplo3_opencv/build/CMakeFiles/camvidCV.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/camvidCV.dir/main.cpp.o"
+  "CMakeFiles/camvidCV.dir/main.cpp.o.d"
+  "camvidCV"
+  "camvidCV.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/camvidCV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
