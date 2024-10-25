56222a4d76c7b9a7203e441e10e383edf198cc55
JuanErazo <42411965+JuanEraz0@users.noreply.github.com>
2024-10-25 02:09:43 -0600

    Delete qml-qt/QObject-memoriadin/QObject-md-parent directory

--- qml-qt/QObject-memoriadin/QObject-md-parent/build/.qt/QtDeployTargets.cmake
+++
@@ -1,1 +0,0 @@
-set(__QT_DEPLOY_TARGET_QObject-md-parent_FILE /home/juan-jose-erazo-pacheco/Desktop/Servicio_Social_LABCOM/qml-qt/QObject-memoriadin/QObject-md-parent/build/QObject-md-parent)
--- qml-qt/QObject-memoriadin/QObject-md-parent/build/CMakeFiles/QObject-md-parent_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/QObject-md-parent_autogen"
-  "QObject-md-parent_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/QObject-md-parent_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
