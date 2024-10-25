3b6036f84c483feca5ad1c49389a9898f5b24c02
JuanjoEraz0 <juanerazo.fi@gmail.com>
2024-10-25 01:55:44 -0600

    Ejemplo y uso de jerarquia de clases con QObject, uso de lambda

---
+++ qml-qt/QObject-memoriadin/QObject-md-parent/build/CMakeFiles/QObject-md-parent.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for QObject-md-parent.
+# This may be replaced when dependencies are built.
---
+++ qml-qt/QObject-memoriadin/QObject-md-parent/build/CMakeFiles/QObject-md-parent_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QObject-md-parent_autogen"
+  "QObject-md-parent_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/QObject-md-parent_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
